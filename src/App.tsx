a2386cf3dfaaec45ebd7ad57ff8ac1c4b7cf5978
ahmad1702 <ahmad1702@gmail.com>
2023-05-26 18:33:06 -0500

    initializing shadcn-ui in cra app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
+import { Button } from './components/ui/button';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <header className="h-screen flex flex-col items-center justify-center">
+        <img src={logo} className="w-[40vmin] animate-[spin_10s_linear_infinite]" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button asChild variant='link'>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </Button>
+        <Button variant='outline' onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
+        </Button>
       </header>
     </div>
   );
